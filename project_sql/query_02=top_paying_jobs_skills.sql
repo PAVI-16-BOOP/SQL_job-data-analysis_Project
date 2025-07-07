323d37c2992e1fba3f8606768ee88b4b86ff9186
Pavitra <vantoffactor15@gmail.com>
2025-07-08 02:32:02 +0530

    query_02_final

--- project_sql/query_02=top_paying_jobs_skills.sql
+++ project_sql/query_02=top_paying_jobs_skills.sql
@@ -1,9 +1,12 @@
 /* 
-Q2) What skills are required for the top paying data analyst/scientist jobs ?
--use the top 1000 highest paying Data Analyst/Scientist roles that are availably remotely and in hybrid mode.
-- add specific skills required for these roles.
--WHY? It provides a detailed look at which skills to develop that align with the highest paying salaries
+Q1) What are the top - paying data analyst/scientist jobs ?
+- identify the top 1000 highest paying Data Analyst/Scientist roles that are availably remotely and in hybrid mode.
+- jobs in specific countries = (Australia, Canada, Germany, United Kingdom, United States, India,Denmark, Netherlands,
+- Sweden, Singapore,France,Hungry,Portugal,Spain,Italy,Poland,Finland)
+- focus on job postings that have the salaries specified (remove nulls)
+WHY? to highlight the top paying jobs in the data analyst/scientist field.
 */
+
 
 WITH top_paying_jobs_skills AS(
 SELECT 
@@ -30,6 +33,13 @@ ORDER BY
     jpf.salary_year_avg DESC
 LIMIT 1000
 ) 
+
+/* 
+Q2) What skills are required for the top paying data analyst/scientist jobs ?
+-use the top 1000 highest paying Data Analyst/Scientist roles that are availably remotely and in hybrid mode.
+- add specific skills required for these roles.
+-WHY? It provides a detailed look at which skills to develop that align with the highest paying salaries
+*/
 
 SELECT 
 tpjs.* , 
